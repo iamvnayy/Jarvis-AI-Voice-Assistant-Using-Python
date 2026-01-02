ce0b3ea81b404dabdedebea766a16eb1b8748ef6
Vinay Kumar <vpal93592@gmail.com>
2026-01-02 13:57:37 +0530

    Update musicLibrary.py

--- musicLibrary.py
+++ musicLibrary.py
@@ -1,6 +1,5 @@
 music = {
-    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
-    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
-    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
-    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21"
+    "sorry":"https://www.youtube.com/watch?v=8ELbX5CMomE&list=RD8ELbX5CMomE&start_radio=1",
+    "despascito":"https://www.youtube.com/watch?v=kJQP7kiw5Fk&list=RDkJQP7kiw5Fk&start_radio=1",
+    "sanson_ki_mala":"https://www.youtube.com/watch?v=bp9YVQcOjgo&list=RDbp9YVQcOjgo&start_radio=1"
 }
